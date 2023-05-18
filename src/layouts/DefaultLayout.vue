976d61f290611b787290cb122e75d601e7e12646
Chaesung Lim <estail7s@nexon.co.kr>
2023-05-18 13:29:30 +0900

    + init

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="content">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss">
+html body {
+  min-width: 100vw;
+  min-height: 100vh;
+  background: #fdfd96;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <DefaultLayout>
+      <div class="icon">😊</div>
+    </DefaultLayout>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    DefaultLayout,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  font-size: 5rem;
+  text-align: center;
+  padding-top: 30vh;
+}
+</style>
